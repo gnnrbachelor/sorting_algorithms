8876c47a03f483a0d9993403ce318151a8365467
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-12 17:11:43 +0000

    Fix bubble_sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,21 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps for sort
+ * @var1: Variable 1
+ * @var2: Variable 2
+ * Return: Void
+ */
+
+void swap(int *var1, int *var2)
+{
+	int temp = *var1;
+
+	*var1 = *var2;
+	*var2 = temp;
+}
+
+/**
  * bubble_sort - Sorts Array
  * @array: Array
  * @size: Size
