cbf174c6152043d611327bcf594931cb5556073e
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-12 18:03:19 +0000

    Fixing null problem

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -28,6 +28,9 @@ void bubble_sort(int *array, size_t size)
 	unsigned int i = 0;
 	unsigned int j = 0;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 		for (j = 0; j < size - i - 1; j++)
 		{
