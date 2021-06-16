651bd4b9735a8aa0f280ef0d20b5adcbc61656e8
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-16 23:42:17 +0000

    Add cocktail sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,14 +1,70 @@
 #include "sort.h"
 
 /**
- * shell_sort - Sorts array
- * @array: Array
- * @size: Size
+ * swap_it - swaps two nodes
+ * @list: head
+ * @node: node
+ * Return: Void
+ */
+
+void swap_it(listint_t **list, listint_t *node)
+{
+	if (node->next)
+		node->next->prev = node->prev;
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+		*list = node->next;
+	node->prev = node->next;
+	node->next = node->next->next;
+	node->prev->next = node;
+	if (node->next)
+		node->next->prev = node;
+}
+
+/**
+ * cocktail_sort_list - Sorts array
+ * @list: Head
  * Return: Void
  */
 
 
-void shell_sort(int *array, size_t size)
+void cocktail_sort_list(listint_t **list)
 {
+	int swap = 1;
+	listint_t *temp = *list;
 
+	if (list == NULL || *list == NULL)
+		return;
+
+	while (swap)
+	{
+		swap = 0;
+		while (temp->next != NULL)
+		{
+			if (temp->n > temp->next->n)
+			{
+				swap_it(list, temp);
+				print_list(*list);
+				swap = 1;
+			}
+			else
+				temp = temp->next;
+
+		}
+		if (swap == 0)
+			break;
+		swap = 0;
+		while (temp->prev != NULL)
+		{
+			if (temp->n < temp->prev->n)
+			{
+				swap_it(list, temp->prev);
+				print_list(*list);
+				swap = 1;
+			}
+			else
+				temp = temp->prev;
+		}
+	}
 }
--- sort.h
+++ sort.h
@@ -34,4 +34,8 @@ size_t partition(int *array, ssize_t low, ssize_t high, size_t size);
 void rec_sort(int *array, ssize_t low, ssize_t high, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_it(listint_t **list, listint_t *node);
+
+
 #endif
