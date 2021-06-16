b9d261a1aed50da2e9a08c6433431476b81e1144
gnnrbachelor <gnnrbachelor@gmail.com>
2021-06-16 15:35:25 +0000

    Fix betty

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -16,25 +16,21 @@ void counting_sort(int *array, size_t size)
 
 	if (array == NULL || size < 2)
 		return;
-
 	result = malloc(sizeof(int) * size);
 	if (result == NULL)
 		return;
-
 	for (; i < (int)size; i++)
 	{
 		result[i] = array[i];
 		if (array[i] > max)
 			max = array[i];
 	}
-
 	count = malloc(sizeof(int) * max + 1);
 	if (count == NULL)
 	{
 		free(result);
 		return;
 	}
-
 	for (i = 0; i <= max; i++)
 		count[i] = 0;
 	for (i = 0; i < (int)size; i++)
@@ -42,16 +38,13 @@ void counting_sort(int *array, size_t size)
 	for (i = 0; i <= max; i++)
 		count[i] += count[i - 1];
 	print_array(count, max + 1);
-
 	for (i = 0; i < (int)size; i++)
 	{
 		result[count[array[i]] - 1] = array[i];
 		--count[array[i]];
 	}
-
 	for (i = 0; i < (int)size; i++)
 		array[i] = result[i];
-
 	free(result);
 	free(count);
 }
